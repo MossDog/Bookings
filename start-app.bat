129489fe9c202ce72c99074f14400f9d1c2fd52c
MossDog <lukejohnmosshughes@gmail.com>
2025-05-30 20:32:52 +0100

    Add start-app.bat script to launch server and web client

---
+++ start-app.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Navigate to the server directory and start the server
+start cmd /k "cd /d server && npm start"
+
+:: Navigate to the web-client directory and start the client
+start cmd /k "cd /d web-client && npm run dev"
