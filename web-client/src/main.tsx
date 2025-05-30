3c7c4a15554121f2ee839d1e4f564ebfe135ee5f
conorbowles51 <conorbowles2000@gmail.com>
2025-05-30 19:42:42 +0100

    Added react router dom. Set up browser router. Created ProtectedRoute component.

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './index.css'
-import App from './App.tsx'
+
+import HomePage from './pages/HomePage.tsx'
+import LoginPage from './pages/auth/LoginPage.tsx'
+import SignUpPage from './pages/auth/SignUpPage.tsx'
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/sign-up", element: <SignUpPage /> },
+  { path: "/login", element: <LoginPage /> }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
