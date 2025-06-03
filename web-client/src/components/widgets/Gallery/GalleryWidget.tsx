38a0f09edbf4ab568680a420db4c1daa2a7abe80
conorbowles51 <conorbowles2000@gmail.com>
2025-06-03 19:45:48 +0100

    Confirm email page and installed daisyUI

--- web-client/src/components/widgets/Gallery/GalleryWidget.tsx
+++ web-client/src/components/widgets/Gallery/GalleryWidget.tsx
@@ -3,10 +3,28 @@ import GallerySlot from './GallerySlot'
 
 export default function GalleryWidget() {
   return (
-    <div className='m-6 border w-[600px] h-[600px] grid gap-2 grid-cols-3 grid-rows-3'>
+    <div className='w-[600px] h-[600px] grid gap-2 grid-cols-3 grid-rows-3'>
       <GallerySlot path='/test_Restaurant.jpeg' x={1} y={1} height={2} width={2}/>
       <GallerySlot x={3} height={2}/>
       <GallerySlot x={1} y={3} width={3} />
     </div>
   )
 }
+
+/*
+  Registration
+  -------------
+  Description
+  address
+  Business name
+  Business category
+  
+
+  Post-registration
+  -----------------
+  Availability!!!
+    --- Recurring (monthly, weekly)
+    --- manual
+  Services and prices
+  Images
+*/
---
+++ web-client/src/pages/ConfirmEmailPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ConfirmEmailPage() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center tracking-wider'>
+      Please check your email to confirm your sign-up.
+    </div>
+  )
+}
