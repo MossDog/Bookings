4c085336e3665d1c1b9c63a1198ff131b6c7e2d9
conorbowles51 <conorbowles2000@gmail.com>
2025-06-11 21:02:42 +0100

    feat: implement service management in seller profile setup

--- web-client/src/pages/seller/profile-creation/SellerProfileSetupPage.tsx
+++ web-client/src/pages/seller/profile-creation/SellerProfileSetupPage.tsx
@@ -2,12 +2,15 @@ import React, { useEffect, useRef, useState } from "react";
 import HorizontalSteps from "../../../components/HorizontalSteps";
 import SellerProfileCreationForm from "../../../components/seller/profile-creation/SellerProfileCreationForm";
 import Navbar from "@/components/Navbar";
-import { ProfileCreationData } from "@/utils/sellerProfileUtils";
+import { createSellerProfile, ProfileCreationData } from "@/utils/sellerProfileUtils";
 import { useUser } from '@supabase/auth-helpers-react'
+import SellerServicesSetup from "@/components/seller/profile-creation/SellerServicesSetup";
+import { Service } from "@/types/types";
 
 function SellerProfileSetupPage() {
   const user = useUser();
   const [profileData, setProfileData] = useState<ProfileCreationData>();
+  const [services, setServices] = useState<Service[]>([]);
   const [isFormValid, setIsFormValid] = useState(false);
 
   const handleValidFormData = (data: ProfileCreationData) => {
@@ -19,6 +22,13 @@ function SellerProfileSetupPage() {
     setIsFormValid(false);
   }
 
+  const handleNewService = (service: Service) => {
+    setServices([
+      ...services,
+      service
+    ])
+  }
+
   const validateStep = (stepIndex: number) => {
     if (stepIndex === 0) {
       return isFormValid;
@@ -28,7 +38,32 @@ function SellerProfileSetupPage() {
   };
 
   const onSubmit = async () => {
-    
+    if (!user || !profileData) {
+      return;
+    }
+
+    // Destructure the required fields to ensure they exist
+    const { name, description, address, category } = profileData;
+
+    // Check if all required fields are present
+    if (!name || !description || !address || !category) {
+      return;
+    }
+
+    try {
+      const { success } = await createSellerProfile({
+        ...profileData,
+        services
+      });
+
+      if(success){
+        console.log("Business profile successfully created!");
+      } else {
+        throw new Error("Error creating business");
+      }
+    } catch (error){
+      console.error(error);
+    }
   }
 
 
@@ -36,14 +71,18 @@ function SellerProfileSetupPage() {
     <div className="min-h-screen overflow flex flex-col">
       <Navbar />
       <HorizontalSteps 
-        steps={["Basic Info", "Availability", "Add Images"]}
+        steps={["Basic Info", "Services", "Bookings", "Add Images", "Confirm"]}
         validateStep={validateStep}  
       >
         <SellerProfileCreationForm
           onInvalidData={handleInvalidFormData}
           onValidData={handleValidFormData}
         />
+        <SellerServicesSetup 
+          onNewService={handleNewService}
+        />
         <div>TODO: Availability</div>
+        <div>TODO: Gallery maybe? </div>
         <button className="btn" onClick={onSubmit}>Confirm Account Creation</button>
       </HorizontalSteps>
     </div>
