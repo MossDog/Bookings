1088a4d2d8a1b5682a95c816fe65d4e65dd0eacd
jasonldoyle <jliudoyle01@gmail.com>
2025-06-14 21:37:30 +0100

    fix: remove unused React import in MyBookings component

--- web-client/src/components/MyBookings.tsx
+++ web-client/src/components/MyBookings.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import supabase from "@/utils/supabase";
 import { DateTime } from "luxon";
 import { toast } from "sonner";
